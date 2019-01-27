aa24b3e25242be052aec3eae240981d2fff2b08f
Kim Kern <github@kimkern.de>
2019-01-27 15:38:11 +0100

    add tests for ConfigService

--- src/config/config.service.spec.ts
+++ src/config/config.service.spec.ts
@@ -10,6 +10,7 @@ describe('ConfigService', () => {
     }).compile();
     service = module.get<ConfigService>(ConfigService);
   });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(service.jwtSecret &&
@@ -27,4 +28,23 @@ describe('ConfigService', () => {
       service.telegramToken &&
       !service.isProduction).toBeTruthy();
   });
+
+  it('should parse mongodb variables for heroku', async () => {
+    const dbName = 'heroku_688asJszasbahs';
+    const port = 1234;
+    const password = `jsdjjb7232bjhas7a78`;
+    const host = `fd568656.mydb.kiwi`;
+    process.env.MONGODB_URI = `mongodb://${dbName}:${password}@${host}:${port}/${dbName}`;
+    try {
+      const configService = new ConfigService();
+      expect(configService.databaseHost).toBe(host);
+      expect(configService.databasePort).toBe(port);
+      expect(configService.databasePassword).toBe(password);
+      expect(configService.databaseName).toBe(dbName);
+      expect(configService.databaseUsername).toBe(dbName);
+    } finally {
+      delete process.env.MONGODB_URI;
+    }
+  });
+
 });
