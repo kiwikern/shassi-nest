b0fab511ac56aee5913eaa957ef64b4476db16e0
Kim Kern <github@kimkern.de>
2019-03-02 16:18:36 +0100

    return telegram connection status from telegram controller
    closes #11 closes #10

--- src/telegram/telegram.controller.spec.ts
+++ src/telegram/telegram.controller.spec.ts
@@ -2,25 +2,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TelegramController } from './telegram.controller';
 import { TelegramTokenService } from './telegram-token.service';
 import { NoOpLogger } from '../../test/mocks/no-op-logger';
+import { TelegramUserIdService } from './telegram-user-id.service';
+import { telegramUserIdServiceFactory } from '../../test/mocks/jest-mocks';
+import { UserEntity } from '../users/entities/user.entity';
+import { MockType } from '../../test/mock.type';
 
 describe('Telegram Controller', () => {
   let module: TestingModule;
   let controller: TelegramController;
   const tokenServiceMock = { createToken: () => 'token' };
+  let userIdServiceMock: MockType<TelegramUserIdService>;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     module = await Test.createTestingModule({
       controllers: [TelegramController],
       providers: [
         { provide: TelegramTokenService, useValue: tokenServiceMock },
+        { provide: TelegramUserIdService, useFactory: telegramUserIdServiceFactory },
       ],
     }).compile();
     module.useLogger(new NoOpLogger());
     controller = module.get<TelegramController>(TelegramController);
+    userIdServiceMock = module.get(TelegramUserIdService);
+  });
+
+  it('should return connection status', async () => {
+    userIdServiceMock.findTelegramId.mockReturnValueOnce(null);
+    expect(await controller.getConnectionStatus({ _id: '' } as UserEntity))
+      .toEqual({ isConnectedToTelegram: false });
+  });
+
+  it('should return connection status', async () => {
+    userIdServiceMock.findTelegramId.mockReturnValueOnce('89341298');
+    expect(await controller.getConnectionStatus({ _id: '' } as UserEntity))
+      .toEqual({ isConnectedToTelegram: true });
   });
 
   it('should create a new token', async () => {
-    expect(await controller.createToken({ _id: '' } as any)).toEqual({ token: 'token' });
+    expect(await controller.createToken({ _id: '' } as UserEntity))
+      .toEqual({ token: 'token' });
   });
 
 });
--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,19 +1,31 @@
 import { ClassSerializerInterceptor, Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiForbiddenResponse, ApiUseTags, ApiBadRequestResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCreatedResponse, ApiForbiddenResponse, ApiOkResponse, ApiUseTags } from '@nestjs/swagger';
 import { User } from '../auth/user.decorator';
 import { UserEntity } from '../users/entities/user.entity';
 import { TelegramTokenService } from './telegram-token.service';
+import { TelegramUserIdService } from './telegram-user-id.service';
 
 @Controller('telegram')
 @UseGuards(AuthGuard('jwt'))
 @UseInterceptors(ClassSerializerInterceptor)
 @ApiBearerAuth()
-@ApiUseTags('products')
+@ApiUseTags('telegram')
 @ApiForbiddenResponse({ description: 'User must be logged in.' })
 export class TelegramController {
 
-  constructor(private telegramTokenService: TelegramTokenService) {
+  constructor(private telegramTokenService: TelegramTokenService,
+              private telegramUserIdService: TelegramUserIdService) {
+  }
+
+  @ApiOkResponse({
+    description: 'Returns whether user is connected to telegram bot.',
+    type: { isConnectedToTelegram: Boolean },
+  })
+  @Get()
+  async getConnectionStatus(@User() user: UserEntity): Promise<{ isConnectedToTelegram: boolean }> {
+    const telegramId = await this.telegramUserIdService.findTelegramId(user._id);
+    return { isConnectedToTelegram: !!telegramId };
   }
 
   @ApiCreatedResponse({
