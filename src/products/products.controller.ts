c5d57e230ba0998b7ec4dd28a0c8681502ffac95
Kim Kern <github@kimkern.de>
2019-04-07 20:07:59 +0200

    set swagger param type to string for ObjectId params
    closes #15

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,7 +7,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserEntity } from '../users/entities/user.entity';
 import { InitializeProductDto } from './dtos/initialize-product.dto';
 import { ObjectID } from 'mongodb';
-import { ApiBearerAuth, ApiOkResponse, ApiUseTags, ApiForbiddenResponse, ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiUseTags, ApiForbiddenResponse, ApiCreatedResponse, ApiOperation, ApiImplicitParam } from '@nestjs/swagger';
 import { ProductSizeAvailability } from '../crawler/product-size.interface';
 import { ObjectIdPipe } from '../common/object-id.pipe';
 
@@ -23,14 +23,14 @@ export class ProductsController {
   }
 
   @Get('/')
-  @ApiOperation({title: 'List of products', description: 'Get the products for the logged in user'})
+  @ApiOperation({ title: 'List of products', description: 'Get the products for the logged in user' })
   @ApiOkResponse({ isArray: true, description: 'Returns all the user\' products', type: ProductEntity })
   getAllProducts(@User() user: UserEntity): Promise<ProductEntity[]> {
     return this.productsService.getProducts(user._id);
   }
 
   @Post('/init')
-  @ApiOperation({title: 'Initialize product', description: 'Parse the product data for the given url without saving it in the database.'})
+  @ApiOperation({ title: 'Initialize product', description: 'Parse the product data for the given url without saving it in the database.' })
   @ApiCreatedResponse({
     description: 'Returns the initial data for a product. Not yet saved in the database.',
     type: { sizes: ProductSizeAvailability, name: String, url: String },
@@ -40,29 +40,32 @@ export class ProductsController {
   }
 
   @Post('/')
-  @ApiOperation({title: 'Save product', description: 'Saves a new product to the database with the chosen size.'})
+  @ApiOperation({ title: 'Save product', description: 'Saves a new product to the database with the chosen size.' })
   @ApiCreatedResponse({ description: 'Returns the saved product.', type: ProductEntity })
   addProduct(@User() user: UserEntity, @Body() product: CreateProductDto): Promise<ProductEntity> {
     return this.productsService.addProduct(user._id, product);
   }
 
   @Post('/:id/markread')
-  @ApiOperation({title: 'Mark updates as read', description: 'If the product has unread updates they will be marked as read.'})
+  @ApiOperation({ title: 'Mark updates as read', description: 'If the product has unread updates they will be marked as read.' })
   @ApiCreatedResponse({ description: 'Returns the updated product.' })
+  @ApiImplicitParam({ name: 'id', type: String })
   markRead(@User() user: UserEntity, @Param('id', ObjectIdPipe) productId: ObjectID): Promise<ProductEntity> {
     return this.productsService.markRead(user._id, productId);
   }
 
   @Post('/:id/update')
-  @ApiOperation({title: 'Update single product', description: 'Gets the product updates for the given single product.'})
+  @ApiOperation({ title: 'Update single product', description: 'Gets the product updates for the given single product.' })
   @ApiCreatedResponse({ description: 'Returns the updated product.' })
+  @ApiImplicitParam({ name: 'id', type: String })
   updateProduct(@User() user: UserEntity, @Param('id', ObjectIdPipe) productId: ObjectID): Promise<ProductEntity> {
     return this.productsService.updateSingleProduct(user._id, productId);
   }
 
   @Delete('/:id')
-  @ApiOperation({title: 'Delete product', description: 'Deletes the given product.'})
+  @ApiOperation({ title: 'Delete product', description: 'Deletes the given product.' })
   @ApiOkResponse({ description: 'Returns success state.' })
+  @ApiImplicitParam({ name: 'id', type: String })
   async deleteProduct(@User() user: UserEntity, @Param('id', ObjectIdPipe) productId: ObjectID) {
     return { success: await this.productsService.deleteProduct(user._id, productId) };
   }
