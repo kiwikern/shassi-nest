d90f377dced586596edda8e5f76eae4dfb3ff12e
Kim Kern <github@kimkern.de>
2019-11-24 15:10:57 +0100

    fix asos by adding a cookie to the request

--- src/crawler/crawlers/asos.crawler.ts
+++ src/crawler/crawlers/asos.crawler.ts
@@ -20,6 +20,7 @@ export class AsosCrawler implements Crawler {
       'accept': 'text/html,application/xhtml+xml,application/xmlq=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
       'accept-encoding': 'gzip, deflate, br',
       'accept-language': 'en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7',
+      'Cookie': `ak_bmsc=${Math.random().toString(36).substring(20)}=; geocountry=DE`,
       'cache-control': 'no-cache',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
     };
